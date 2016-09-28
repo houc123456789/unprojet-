fdcf723f32246b06986dcfada37dc8b1c7c36322
yrazafi <yrazafi@D203-2.Tertiaire.loges>
2016-09-28 11:33:10 +0200

    premier Commit du ficher Capitle

---
+++ src/Capitale.java
@@ -0,0 +1,48 @@
+
+public class Capitale /*extends Ville*/{
+
+	private String nomCapitale;
+	private int nbHabitants;
+	private int superficie;
+	
+	public Capitale(String unNomCapitale, int unNbHabitant, int uneSuperficie){
+	this.nomCapitale = unNomCapitale;
+	this.nbHabitants = unNbHabitant;
+	this.superficie = uneSuperficie;
+	}
+	
+	//accesseurs
+	//nomCapitale
+	public String getNomCapitale(){
+		return nomCapitale;
+	}
+	public void setNomCapitale ( String unNomCapitale){
+		this.nomCapitale=unNomCapitale;
+	}
+	
+	//nbHabitants
+	public int getNbHabitant(){
+		return nbHabitants;
+	}
+	
+	public void setNbHabitant( int unNbHabitant){
+		this.nbHabitants=unNbHabitant;
+	}
+	
+	//superficie
+	public int getSuperficie(){
+		return superficie;
+	}
+	public void setSuperficie (int uneSuperficie){
+		this.superficie=uneSuperficie;
+	}
+	
+	//toString
+	public String toString(){
+		String chaine;
+		chaine="\nNom de la capitale:"+ this.nomCapitale+ "\nNombre d'habitant dans la capitale:"+this.nbHabitants+ "\nSuperficie de la capitale:"+this.superficie;
+	
+		return chaine;
+	}
+	
+}
